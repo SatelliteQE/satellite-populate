76884d537fa726be258018e8cadf3d8f83b60433
Bruno Rocha <rochacbruno@gmail.com>
2017-01-12 16:36:40 -0200

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='satellite_populate',
-    version='0.1.1',
+    version='0.1.2',
     description="Populate and Validate the System using YAML",
     long_description=readme + '\n\n' + history,
     author="Bruno Rocha",
